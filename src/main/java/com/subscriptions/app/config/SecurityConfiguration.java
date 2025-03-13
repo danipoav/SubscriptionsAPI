decd8cc7abb846805f0e8a900963b4ee7292b505
danipoav <dapoav2002@gmail.com>
2025-03-13 23:10:00 +0100

    Creating Securityconf for intercept every http request and validate the token before the http request get into the controller

--- src/main/java/com/subscriptions/app/config/SecurityConfiguration.java
+++ src/main/java/com/subscriptions/app/config/SecurityConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,9 +23,15 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-        .authorizeHttpRequests(null)
-        .securityMatcher("")
-        .
+
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll() // Rutas públicas
+                        .anyRequest().authenticated());
 
         return http.build();
     }
