14238af99e66a4552c64bfeaa2ed9fc575a78d86
danipoav <dapoav2002@gmail.com>
2025-03-25 16:01:48 +0100

    Adding cross origin to authcontroller

--- src/main/java/com/subscriptions/app/controller/auth/AuthController.java
+++ src/main/java/com/subscriptions/app/controller/auth/AuthController.java
@@ -1,6 +1,7 @@
 package com.subscriptions.app.controller.auth;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin
 public class AuthController {
 
     private final AuthenticationService authenticationService;
