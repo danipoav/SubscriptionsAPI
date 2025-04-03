539a6df1b8c2ff6e91cc2fb1bd35ae79c1e1965c
danipoav <dapoav2002@gmail.com>
2025-04-03 22:32:41 +0200

    Creating lambda

--- src/main/java/com/subscriptions/app/controller/auth/AuthController.java
+++ src/main/java/com/subscriptions/app/controller/auth/AuthController.java
@@ -2,6 +2,7 @@ package com.subscriptions.app.controller.auth;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,11 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authenticationService.login(request));
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
     }
 
 }
